9e2b24e0fef271e1e223dabc9c71891ed845554e
ramsharma1999 <38228713+ramsharma1999@users.noreply.github.com>
2021-09-15 13:33:35 +0530

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,23 @@
+def sorting(arr):
+    length = len(arr)
+    if length <= 1:
+        return arr
+    else:
+        a = arr.pop()
+        
+    greater_num = []
+    lower_num = []
+    
+    for num in arr:
+        if num>a:
+            greater_num.append(num)
+        else:
+            lower_num.append(num)
+            
+    return sorting(lower_num) + [a] + sorting(greater_num)
+
+print(sorting([2,1,4,10,23,21,3,4,5,0,6,7]))
+
+/*
+jaslslaslasldsd
+*/
