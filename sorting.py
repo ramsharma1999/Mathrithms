f06f4921a4109a44f890d158420948c4f81606e6
ramsharma1999 <38228713+ramsharma1999@users.noreply.github.com>
2021-09-15 14:24:23 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -20,4 +20,12 @@ print(sorting([2,1,4,10,23,21,3,4,5,0,6,7]))
 
 /*
 Algo:
+1. check for the length of array 
+If => length < 1 return array
+else => take the last element as pivot
+2. for every num in array:
+    if => num > pivot, Put num in greater_num[]
+    else => Put num in lower_num[]
+3. return greater_num[] + [pivot] + lower_num
+    
 */
